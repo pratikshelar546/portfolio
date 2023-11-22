40f57a271a59654f5c0eb22ef3c3d1b11616a2bc
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2023-11-22 21:20:15 +0530

    removed loding page

--- src/App.js
+++ src/App.js
@@ -3,30 +3,21 @@ import Home from "./compomends/Home";
 import Navbar from "./compomends/Navbar";
 import Projects from "./compomends/Projects";
 import Skills from "./compomends/Skills";
-import LaodingScreen from "./compomends/LaodingScreen";
+
 
 
 
 function App() {
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
 
-    const timeOut = setTimeout(() => {
-      setLoading(false)
-    }, 2000);
-    return () => clearTimeout(timeOut)
-  })
   return (
-    <>{
-      loading ? <LaodingScreen /> : <>
+   <>
         <Navbar />
         <Home />
         <Skills />
         <Projects />
       </>
-        }
-    </>
+
   );
 }
 
