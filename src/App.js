1860c62b1adab130f413e5503ff7dfa5041be3c5
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2023-11-22 21:22:34 +0530

    fixes bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import Home from "./compomends/Home";
 import Navbar from "./compomends/Navbar";
 import Projects from "./compomends/Projects";
